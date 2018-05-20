86e850935fc9ed741c680b43e3af356ba5dff7d1
KH <kasperkh.kh@gmail.com>
2018-05-21 01:22:28 +0200

    1.1.0 - Fixed multiline string coloring

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function addColor(name, start, end = "\x1b[0m") {
     global.String.prototype.__defineGetter__(name, function() {
         let str = this;
-        str.replace("\n", end+start);
-        if (this.endsWith(end)) str= start+this;
-        else str = start+this+end;
+        str = str.replace("\n", end+"\n"+start);
+        str = start+str+end;
         return str;
     });
 }
@@ -40,3 +39,29 @@ module.exports = (ignore = false, customColors = []) => {
         }
     }
 }
+
+// Reset = "\x1b[0m"
+// Bright = "\x1b[1m"
+// Dim = "\x1b[2m"
+// Underscore = "\x1b[4m"
+// Blink = "\x1b[5m"
+// Reverse = "\x1b[7m"
+// Hidden = "\x1b[8m"
+//
+// FgBlack = "\x1b[30m"
+// FgRed = "\x1b[31m"
+// FgGreen = "\x1b[32m"
+// FgYellow = "\x1b[33m"
+// FgBlue = "\x1b[34m"
+// FgMagenta = "\x1b[35m"
+// FgCyan = "\x1b[36m"
+// FgWhite = "\x1b[37m"
+//
+// BgBlack = "\x1b[40m"
+// BgRed = "\x1b[41m"
+// BgGreen = "\x1b[42m"
+// BgYellow = "\x1b[43m"
+// BgBlue = "\x1b[44m"
+// BgMagenta = "\x1b[45m"
+// BgCyan = "\x1b[46m"
+// BgWhite = "\x1b[47m"
